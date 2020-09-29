ee498694202e7868bc8588ee58b749293c671044
DPU J S <DPU J S@DESKTOP-12TU7QI>
2020-09-29 11:10:06 +0530

    Add project files.

---
+++ LinearSearch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        public static int Search(int[] a,int n,int searchValue)
+        {
+            for(int i=0; i < n; i++)
+            {
+                if (a[i] == searchValue)
+                    return i;
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int i, n, searchValue, index;
+            int[] a = new int[100];
+
+            Console.Write("Enter the number of elements : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Entee the elements - ");
+            for (i = 0; i < n; i++)
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the search value : ");
+            searchValue = Convert.ToInt32(Console.ReadLine());
+            index = Search(a, n, searchValue);
+            if (index == -1)
+                Console.WriteLine(" value " + searchValue + " not present in the array\n ");
+            else
+                Console.WriteLine(" value " + searchValue + " present at index " + index);
+        }
+    }
+}
